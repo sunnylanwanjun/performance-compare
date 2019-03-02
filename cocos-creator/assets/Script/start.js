1008363f819b307bedf91452d42c4d1d1a844e0c
caryliu <caryliu1999@gmail.com>
2019-03-02 18:55:58 +0800

    add egret text and scrollview test

--- cocos-creator/assets/Script/start.js
+++ cocos-creator/assets/Script/start.js
@@ -13,14 +13,20 @@ cc.Class({
         switch(sceneType)
         {
             case 'sheep':
-            cc.director.loadScene('sheep');
-            break;
+                cc.director.loadScene('sheep');
+                break;
             case 'spine':
-            cc.director.loadScene('spine');
-            break;
+                cc.director.loadScene('spine');
+                break;
             case 'dragonbones':
-            cc.director.loadScene('dragonbones');
-            break;
+                cc.director.loadScene('dragonbones');
+                break;
+            case 'progress-text':
+                cc.director.loadScene('progress-text');
+                break;
+            case 'scrollview':
+                cc.director.loadScene('scrollview');
+                break;
         }
     }
 });
